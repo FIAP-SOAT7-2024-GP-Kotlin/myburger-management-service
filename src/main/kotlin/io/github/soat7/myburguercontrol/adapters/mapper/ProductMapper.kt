f9ec44d5e2760e23eb38ccae1cd939941cc2a561
Andre Santos <andrepostech@gmail.com>
2024-11-25 23:13:11 -0300

    Removed order from ProductMapper.kt

--- src/main/kotlin/io/github/soat7/myburguercontrol/adapters/mapper/ProductMapper.kt
+++ src/main/kotlin/io/github/soat7/myburguercontrol/adapters/mapper/ProductMapper.kt
@@ -3,17 +3,11 @@ package io.github.soat7.myburguercontrol.adapters.mapper
 import io.github.soat7.myburguercontrol.domain.entities.Product
 import io.github.soat7.myburguercontrol.domain.entities.enum.ProductType
 import io.github.soat7.myburguercontrol.external.db.product.entity.ProductEntity
-import io.github.soat7.myburguercontrol.external.webservice.order.api.OrderItemResponse
 import io.github.soat7.myburguercontrol.external.webservice.product.api.ProductCreationRequest
 import io.github.soat7.myburguercontrol.external.webservice.product.api.ProductResponse
 import java.time.Instant
 import java.util.UUID
 
-fun Product.toOrderItemProductResponse() = OrderItemResponse.OrderItemProductResponse(
-    name = this.name,
-    productId = this.id,
-    price = this.price,
-)
 
 fun Product.toResponse() = ProductResponse(
     id = this.id,
